5b3096bc19d790fd070c05c6defa8b41f452e259
fariddzenetov <fariddjenetov75@gmail.com>
2025-05-18 23:27:45 +0300

    up

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,16 +1,17 @@
-from django.urls import path
 from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenBlacklistView
 
-from .views import UserModelViewSet, WarehouseModelViewSet, ProductsModelViewSet, \
-    DelModelViewSet
+from .views import (
+    DelModelViewSet,
+    ProductsModelViewSet,
+    UserModelViewSet,
+    WarehouseModelViewSet,
+)
 
 router = DefaultRouter()
-router.register('users', UserModelViewSet)
-router.register('warehouses', WarehouseModelViewSet)
-router.register(r'products', ProductsModelViewSet, basename='products')
-router.register(r'del', DelModelViewSet, basename='del')
-urlpatterns = [
-]
+router.register("users", UserModelViewSet)
+router.register("warehouses", WarehouseModelViewSet)
+router.register(r"products", ProductsModelViewSet, basename="products")
+router.register(r"del", DelModelViewSet, basename="del")
+urlpatterns = []
 
 urlpatterns.extend(router.urls)
--- store/urls.py
+++ store/urls.py
@@ -14,15 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from rest_framework.authtoken.views import obtain_auth_token
 
+from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include("myapp.urls")),
-    path('api-auth/', include('rest_framework.urls')),
-
-
+    path("admin/", admin.site.urls),
+    path("", include("myapp.urls")),
+    path("api-auth/", include("rest_framework.urls")),
 ]
